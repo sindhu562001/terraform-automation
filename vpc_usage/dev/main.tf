36f7830eabd661ed5201c18e84cb8513f221fe26
sindhu562001 <155700200+sindhu562001@users.noreply.github.com>
2025-08-21 11:35:57 -0500

    Update main.tf

--- vpc_usage/dev/main.tf
+++ vpc_usage/dev/main.tf
@@ -10,21 +10,21 @@ module "vpc" {
   subnets = {
     subnet-vpc-a = {
       region        = "us-central1"
-      ip_cidr_range = "10.0.0.0/24"
+      ip_cidr_range = "10.1.0.0/24"
       secondary_ranges = [
       {
         range_name    = "pods-range"
-        ip_cidr_range = "10.20.0.0/16"
+        ip_cidr_range = "10.30.0.0/16"
       },
       {
         range_name    = "services-range"
-        ip_cidr_range = "10.30.0.0/20"
+        ip_cidr_range = "10.40.0.0/20"
       }
     ]
     },
     subnet-vpc-b = {
       region        = "us-central1"
-      ip_cidr_range = "10.0.1.0/24"
+      ip_cidr_range = "10.2.2.0/24"
     }
   }
 }
